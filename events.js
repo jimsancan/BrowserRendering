c2caca914f1e34fb631ab930fb059708e6e95eab
James Sanders-Cannon <James.Cannon@viagogo.com>
2019-07-17 18:08:13 -0400

    timeout

--- events.js
+++ events.js
@@ -1,14 +1,15 @@
 let step = 1;
 console.log(document.readyState + `step ${step++}`);
 
-const domLoaded = () => {
-    console.log(`DOMContentLoaded = step ${step++}`);
-};
-
 document.onreadystatechange = () => {
     console.log(`${document.readyState} step ${step++}`);
 };
 
-setTimeout(() => {console.log('timeout')}, 4000)
+const timeout = setTimeout(() => {console.log('timeout')}, 4000);
+
+const domLoaded = () => {
+    console.log(`DOMContentLoaded = step ${step++}`);
+};
+
 document.addEventListener('DOMContentLoaded', domLoaded);
 
